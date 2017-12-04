2f3eddcc9acfc7f9a36108353b2abc5268d4f898
ShoIwase <i.shokun1021@gmail.com>
2017-12-05 00:35:57 +0900

    ファイル追加

---
+++ image.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import sys, json
+
+for line in sys.stdin:
+  try:
+    tweet = json.loads(line)
+    #リツイート除外
+    if 'retweeted_status' not in tweet:
+       #日本語以外除外
+       if tweet['user']['lang'] == 'ja':
+          #画像つき以外除外
+          if tweet['entities']['media'][0] is None:
+              pass
+          else:
+             screenName = tweet['user']['screen_name']
+             bio = tweet['user']['description'].encode('utf-8').replace("\n", "").replace(",", "")
+             text = tweet['text'].encode('utf-8').replace("\n", "").replace(",", "")
+             imageUrl = tweet['entities']['media'][0]['media_url_https']
+              
+             print screenName,',',
+             print bio,',',
+             print text,',',
+             print imageUrl
+          
+  except StandardError:
+    pass
